39753f6dbc5ad63967f827eb3843d2c8f29354ea
Maria Amusina <>
2020-11-28 16:05:26 +0300

    Add transition from MainActivity to MovieDetailsActivity

--- app/src/main/java/com/example/androidacademymsk/MainActivity.kt
+++ app/src/main/java/com/example/androidacademymsk/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.androidacademymsk
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goToDetailsButton: TextView = findViewById(R.id.first_activity_text_view)
+        goToDetailsButton.setOnClickListener { goToDetails() }
+    }
+
+    private fun goToDetails() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
