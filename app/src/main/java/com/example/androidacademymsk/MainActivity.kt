8e1f5053f09621da37b5161d0cc8f1fde2e78f76
Maria Amusina <>
2020-11-28 17:32:33 +0300

    Add FragmentMoviesList to MainActivity

--- app/src/main/java/com/example/androidacademymsk/MainActivity.kt
+++ app/src/main/java/com/example/androidacademymsk/MainActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentMoviesList = FragmentMoviesList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .apply {
+                add(R.id.fragments_container, fragmentMoviesList)
+                commit()
+            }
     }
 }
