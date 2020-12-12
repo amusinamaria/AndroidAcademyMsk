0589f89c6a3ebcd8a4f725dd41034d89ffae85e3
Maria Amusina <>
2020-12-12 04:31:46 +0300

    Add url to CastCard. Add fake links to MockDataSource. Add Internet permission

--- app/src/main/java/com/example/androidacademymsk/MockDataSource.kt
+++ app/src/main/java/com/example/androidacademymsk/MockDataSource.kt
@@ -72,13 +72,34 @@ class MockDataSource {
 
     fun getCastCards(): List<CastCard> {
         return listOf(
-            CastCard("Justin Dru Bieber"),
-            CastCard("Mark Ruffalo"),
-            CastCard("Robert Downey Jr."),
-            CastCard("Chris Evans"),
-            CastCard("Blake Jenner"),
-            CastCard("Benedict Cumberbatch"),
-            CastCard("Chris Hemsworth"),
+            CastCard(
+                "Justin Dru Bieber",
+                "https://upload.wikimedia.org/wikipedia/commons/d/d9/Robin_Wright_Cannes_2017_%28cropped%29.jpg"
+            ),
+            CastCard(
+                "Mark Ruffalo",
+                "https://upload.wikimedia.org/wikipedia/commons/2/2c/Connie_Nielsen_by_Gage_Skidmore.jpg"
+            ),
+            CastCard(
+                "Robert Downey Jr.",
+                "https://st.kp.yandex.net/im/kadr/1/2/4/kinopoisk.ru-Kenneth-Branagh-1241673.jpg"
+            ),
+            CastCard(
+                "Chris Evans",
+                "https://upload.wikimedia.org/wikipedia/commons/c/c5/Pedro_Pascal_by_Gage_Skidmore.jpg"
+            ),
+            CastCard(
+                "Blake Jenner",
+                "https://upload.wikimedia.org/wikipedia/commons/8/84/David_Harbour_by_Gage_Skidmore.jpg"
+            ),
+            CastCard(
+                "Benedict Cumberbatch",
+                "https://upload.wikimedia.org/wikipedia/commons/7/7f/Rachel_Weisz_2018.jpg"
+            ),
+            CastCard(
+                "Chris Hemsworth",
+                "https://toronto.citynews.ca/wp-content/blogs.dir/sites/10/2019/06/NYET414-618_2019_013921.jpg"
+            ),
         )
     }
 }
