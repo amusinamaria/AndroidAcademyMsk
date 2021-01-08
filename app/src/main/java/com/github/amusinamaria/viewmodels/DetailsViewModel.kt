393f03c0894cdc6ef8d6b15dca4069f724d6fd9b
Maria Amusina <>
2021-01-08 04:13:12 +0300

    WIP: Create DetailsViewModel with fake data loading

---
+++ app/src/main/java/com/github/amusinamaria/viewmodels/DetailsViewModel.kt
@@ -0,0 +1,27 @@
+package com.github.amusinamaria.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.amusinamaria.repository.data.Movie
+import kotlinx.coroutines.launch
+
+class DetailsViewModel @ViewModelInject constructor(val currentMovie: Movie) :
+    ViewModel() {
+
+    private var _mutableMovieDetails = MutableLiveData<Movie>()
+    val movieDetails: LiveData<Movie> get() = _mutableMovieDetails
+
+    init {
+        fetchNewMovieDetails()
+    }
+
+    private fun fetchNewMovieDetails() {
+        viewModelScope.launch {
+            val newMovieDetails = currentMovie
+            _mutableMovieDetails.postValue(newMovieDetails)
+        }
+    }
+}
